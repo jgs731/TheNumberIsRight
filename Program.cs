2fe10d7d85aad4add29b84af5da6f676b12d1932
Joshua Smith <jgs73@live.co.uk>
2022-02-19 11:53:39 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TheNumberIsRight // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public int GenerateNumber()
+        {
+            Random random = new Random();
+            return random.Next(1,100);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
