6df6e5de50c4c08a67cb47e13d2b9b0ab5848ba7
Joshua Smith <jgs73@live.co.uk>
2022-02-19 12:55:15 +0000

    Added main game logic - Removed Console logging of randomly generated number at the start of the game - Game currently has unlimited guesses - Added `sleep` method to add short delay after the welcome message - Use boolean value `correctAnswer` (defaulted to false) - When the user guesses the correct answer this value is set to true and user will exit the while-loop

--- Program.cs
+++ Program.cs
@@ -16,7 +16,31 @@ namespace TheNumberIsRight // Note: actual namespace depends on the project name
         static void Main(string[] args)
         {
             RandomNum rNum = new RandomNum();
-            Console.WriteLine(rNum.generateNumber());
+            int playerGuess;
+            bool correctAnswer = false;
+            
+            Console.WriteLine("Welcome to \'The Number is Right\' game!");
+            Console.WriteLine("------------------------------------");
+            Thread.Sleep(1000);
+
+            while (correctAnswer == false)
+            {
+                Console.Write("Guess todays number (between 1-100): ");
+                int randomGeneratedNumber = rNum.generateNumber();
+                playerGuess = Convert.ToInt32(Console.ReadLine());
+                {
+                    if (playerGuess == randomGeneratedNumber)
+                    {
+                        Console.WriteLine("Correct answer!");
+                        correctAnswer = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect answer. Please try again.");
+                    }
+                }
+            }
+            Console.WriteLine("Thanks for playing!");
         }
     }
 }
